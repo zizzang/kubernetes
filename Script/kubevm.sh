21a19be5f77b42810d1da741df4f7c9493316ebb
shpar <sadla@asndkasd.com>
2022-09-30 09:30:14 +0900

    v1

---
+++ Script/kubevm.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+# Create Resource Group
+az group create \
+    --name dolphin \
+    --location koreacentral
+# Create VM
+az vm create \
+    --resource-group dolphin \
+    --name master \
+    --size Standard_D2s_v3 \
+    --image UbuntuLTS \
+    --location koreacentral \
+    --storage-sku Standard_LRS \
+    --authentication-type password \
+    --admin-username dolphin \
+    --admin-password Azuremsp12#$ \
+    --public-ip-sku basic \
+    --custom-data cloud-init.txt
+# 80 Port Open
+az vm open-port --resource-group dolphin --name master --port 80
